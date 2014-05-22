77ec487b8f158d660df5e7460d2f7aa7717ceafa
Ian Lozinski <ian.lozinski@gmail.com>
2014-05-22 01:06:48 -0400

    basic functionality

--- app.rb
+++ app.rb
@@ -1,11 +1,25 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'haml'
 
 get '/' do
   haml :index
 end
 
-get '/p/:id' do
-  @id = params['id']
-  haml :paste
+post '/' do
+  doc = params['content']
+  puts doc.class
+  File.open("clips/#{doc.hash}", 'w') { |f| f.write(doc) }
+  doc.hash.to_s
+end
+
+get '/:id' do
+  path = "clips/#{params['id']}"
+  if File.exists?(path)
+    @buf = File.open(path) { |f| f.read }
+    File.delete(path)
+    haml :paste
+  else
+    haml :nopaste
+  end
 end
